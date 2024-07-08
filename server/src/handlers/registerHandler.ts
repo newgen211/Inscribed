479dd2bcbfeee9e00de0eac036a2734aa6d21188
Evan Kasky <ekasky25@gmail.com>
2024-07-08 14:38:52 -0400

    Adds register user endpoint functionality
    	- If the data is good to be inserted to the db then the register
    	  endpoint is ran
    
    	- First it checks if the username and password are taken
    
    	- If everything is good, the password gets hashed
    
    	- The user object is created
    
    	- The user is saved to the database

--- server/src/handlers/registerHandler.ts
+++ server/src/handlers/registerHandler.ts
@@ -1,7 +1,71 @@
+import * as argon2 from 'argon2';
 import { Request, Response } from 'express';
+import { User } from '../models/User';
+import { UserDocument } from '../types/ModelTypes';
+import { RegisterUserFields } from '../types/validationTypes';
+import { APIResponse } from '../types/ResponseTypes';
 
-const registerHandler = (req: Request, res: Response): void => {
+const registerHandler = async (req: Request, res: Response): Promise<void> => {
   try {
+    // Extract the register feields from the request body
+    const {
+      first_name,
+      last_name,
+      username,
+      email,
+      password,
+      confirm_password,
+    }: RegisterUserFields = req.body;
+
+    // See if the username and/or email is already taken
+    const existingUser: UserDocument | null = await User.findOne({
+      $or: [{ email }, { username }],
+    });
+
+    if (existingUser) {
+      const response: APIResponse = {
+        timestamp: Date.now(),
+        message: 'Conflict error',
+        code: 409,
+        errors: [],
+      };
+
+      if (existingUser.email === email) {
+        response.errors?.push({ field: 'email', message: 'Email is taken' });
+      }
+
+      if (existingUser.username === username) {
+        response.errors?.push({ field: 'username', message: 'Username taken' });
+      }
+
+      res.status(409).json(response);
+      return;
+    }
+
+    // Hash the user's password for safe stroage
+    const hash = await argon2.hash(password);
+
+    // Create the new user
+    const user: UserDocument = new User({
+      first_name,
+      last_name,
+      username,
+      email,
+      password: hash,
+    });
+
+    // Save the new user to the database
+    await user.save();
+
+    // Return a response message
+    const response: APIResponse = {
+      timestamp: Date.now(),
+      message: 'User successfully registered',
+      code: 201,
+    };
+
+    res.status(201).json(response);
+    return;
   } catch (error) {}
 };
 
