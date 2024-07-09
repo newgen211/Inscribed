4bbccef35917b94fc3fb01a42c31aa720e6eb73c
Evan Kasky <ekasky25@gmail.com>
2024-07-09 08:29:11 -0400

    Extracts register form
    	- Register Form gets its own component
    
    	- Register schema gets its own file

---
+++ client/src/components/RegisterForm/RegisterForm.tsx
@@ -0,0 +1,151 @@
+import { Avatar, Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
+import { Link as ReactRouterDomLink } from 'react-router-dom';
+import AlternateEmailIcon from '@mui/icons-material/AlternateEmail';
+
+const RegisterForm: React.FC = () => {
+
+    return (
+
+        <Container component='main' maxWidth="xs">
+
+            <Box
+                sx={{
+                    marginTop: 8,
+                    display: 'flex',
+                    flexDirection: 'column',
+                    alignItems: 'center',
+                }}
+            >
+
+                {/* Icon and Title */}
+                <Avatar
+                    sx={{
+                        m: 1,
+                        bgcolor: 'secondary.main'
+                    }}
+                >
+                    <AlternateEmailIcon />
+                </Avatar>
+
+                <Typography component='h1' variant='h5'>Sign Up</Typography>
+
+                {/* Sign Up Form */}
+                <Box component='form' noValidate sx={{mt: 3}}>
+
+                    <Grid container spacing={2}>
+
+                        <Grid item xs={12} sm={6}>
+
+                            <TextField 
+                                required
+                                fullWidth
+                                id='first_name'
+                                label='First Name'
+                                name='first_name'
+                                autoComplete="given-name"
+                                autoFocus
+                            />
+
+                        </Grid>
+
+                        <Grid item xs={12} sm={6}>
+
+                            <TextField 
+                                required
+                                fullWidth
+                                id='last_name'
+                                label='Last Name'
+                                name='last_name'
+                                autoComplete="family-name"
+                            />
+
+                        </Grid>
+
+                        <Grid item xs={12}>
+
+                            <TextField 
+                                required
+                                fullWidth
+                                id='username'
+                                label='Username'
+                                name='username'
+                                autoComplete="username"
+                            />
+
+                        </Grid>
+
+                        <Grid item xs={12}>
+
+                            <TextField 
+                                required
+                                fullWidth
+                                id='email'
+                                label='Email Address'
+                                name='email'
+                                autoComplete="email"
+                                type='email'
+                            />
+
+                        </Grid>
+
+                        <Grid item xs={12}>
+
+                            <TextField 
+                                required
+                                fullWidth
+                                id='password'
+                                label='Password'
+                                name='password'
+                                autoComplete="password"
+                                type='password'
+                            />
+
+                        </Grid>
+
+                        <Grid item xs={12}>
+
+                            <TextField 
+                                margin='normal'
+                                required
+                                fullWidth
+                                id='confirm_password'
+                                label='Confirm Password'
+                                name='confirm_password'
+                                autoComplete="confirm_password"
+                                type='password'
+                            />
+
+                        </Grid>
+
+                    </Grid>
+
+                    <Button
+                        type='submit'
+                        fullWidth
+                        variant='contained'
+                        sx={{my: 2}}
+                    >
+                        Sign Up
+                    </Button>
+
+                    <Grid container>
+
+                        <Grid item>
+                            <Link component={ReactRouterDomLink} to='/' variant='body2'>
+                                Already have an account? Sign In
+                            </Link>
+                        </Grid>
+
+                    </Grid>
+
+                </Box>
+
+            </Box>
+
+        </Container>
+
+    );
+
+}
+
+export default RegisterForm;
