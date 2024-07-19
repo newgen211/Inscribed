3d4fbe6b650925646d08eeb5eeb1ca371cbceedf
Evan Kasky <ekasky25@gmail.com>
2024-07-19 16:01:58 -0400

    Adds Dashboard drawer

---
+++ client/src/components/Dashboard/DashboardDrawer.tsx
@@ -0,0 +1,226 @@
+import { Avatar, Box, Divider, IconButton, List, ListItem, ListItemButton, ListItemIcon, ListItemText, styled, Drawer as MuiDrawer } from '@mui/material';
+import { IUserInfo } from '../../pages/Dashboard';
+import { CSSObject, Theme, useTheme } from '@mui/material/styles';
+import { ChevronLeft, ChevronRight, Home, Search, PeopleAlt, Public, Settings, Create } from '@mui/icons-material';
+
+/* Define Types and interfaces */
+export interface IDashboardDrawerProps {
+    drawerOpen       : boolean;
+    setDrawerOpen    : (drawerOpen: boolean) => void;
+    currentTab       : number;
+    setCurrentTab    : (currentTab: number) => void;
+    handleTabClick   : (tab_number: number) => void;
+    fetchingUserInfo : boolean;
+    userInfo         : IUserInfo;
+    fetchUserData    : () => void;
+    getInitals       : (first_name: string, last_name: string) => string;
+};
+
+interface ITab {
+    drawerOpen     : boolean;
+    currentTab     : number;
+    setCurrentTab  : (currentTab: number) => void;
+    handleTabClick : (tab_number: number) => void;
+    tab            : { title: string, icon: any, tab_number: number }
+}
+
+/* Set the width of the drawer */
+const drawerWidth = 240;
+
+/* Main section tab json */
+const mainSectionTabs = [
+
+    {
+        title      : "Home",
+        icon       :  <Home />,
+        tab_number : 0
+    },
+    
+    {
+        title      : "Search",
+        icon       : <Search />,
+        tab_number : 1
+    },
+    
+    {
+        title      : "Following Feed",
+        icon       : <PeopleAlt />,
+        tab_number : 2
+    },
+    
+    {
+        title      : "Discover Feed",
+        icon       : <Public />,
+        tab_number : 3
+    },
+    
+    {
+        title      : "User Settings",
+        icon       : <Settings />,
+        tab_number : 4
+    },
+
+];
+
+/* Mixins */
+export const openedMixin = (theme: Theme): CSSObject => ({
+
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+
+});
+  
+export const closedMixin = (theme: Theme): CSSObject => ({
+
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: `calc(${theme.spacing(7)} + 1px)`,
+    [theme.breakpoints.up('sm')]: {
+      width: `calc(${theme.spacing(8)} + 1px)`,
+    },
+
+});
+
+/* Custom Drawer Component */
+const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    
+    ({ theme, open }) => ({
+
+      width: drawerWidth,
+      flexShrink: 0,
+      whiteSpace: 'nowrap',
+      boxSizing: 'border-box',
+
+      ...(open && {
+        ...openedMixin(theme),
+        '& .MuiDrawer-paper': openedMixin(theme),
+      }),
+
+      ...(!open && {
+        ...closedMixin(theme),
+        '& .MuiDrawer-paper': closedMixin(theme),
+
+      }),
+    })
+
+);
+
+/* Drawer Header Custom Component */
+export const DrawerHeader = styled('div')(({ theme }) => ({
+
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    padding: theme.spacing(0, 1),
+    ...theme.mixins.toolbar
+
+}));
+
+export default function DashboardDrawer(props:IDashboardDrawerProps) {
+
+    /* Get theme object to access its properties */
+    const theme = useTheme();
+
+    return (
+
+        <Drawer variant='permanent' open={props.drawerOpen}>
+
+            {/* Open and Closing button for the drawer */}
+
+            <DrawerHeader>
+
+                {/* Icon button to call open and close drawer */}
+                <IconButton onClick={() => props.setDrawerOpen(false)}>{theme.direction === 'rtl' ? <ChevronRight /> : <ChevronLeft />}</IconButton>
+
+            </DrawerHeader>
+
+            {/* Divider */}
+
+            <Divider />
+
+            {/* Main Tab Section */}
+
+            <List>
+
+                {
+
+                    mainSectionTabs.map((tab, key) => <Tab drawerOpen={props.drawerOpen} currentTab={props.currentTab} setCurrentTab={props.setCurrentTab} handleTabClick={props.handleTabClick} tab={tab} />)
+
+                }
+
+            </List>
+
+            {/* Push all conent to bottom and then habe a divider Divider */}
+
+            <Box sx={{ flexGrow: 1 }} />
+            <Divider />
+
+            {/* New Post Section */}
+            <List>
+
+                <ListItem disablePadding sx={{display: 'block'}}>
+
+                    <ListItemButton sx={{ minHeight: 48, justifyContent: props.drawerOpen ? 'initial': 'center', px: 2.5 }}>
+
+                        <ListItemIcon sx={{ minWidth: 0, mr: props.drawerOpen ? 3 : 'auto', justifyContent: 'center' }}><Create /></ListItemIcon>
+                        <ListItemText primary='New Post' sx={{ opacity: props.drawerOpen ? 1 : 0 }} />
+
+                    </ListItemButton>
+
+                </ListItem>
+
+            </List>
+
+            {/* Divider */}
+            
+            <Divider />
+
+            {/* Display username */}
+            <List>
+
+                <ListItem disablePadding sx={{display: 'block'}}>
+
+                    <ListItemButton sx={{ minHeight: 48, justifyContent: props.drawerOpen ? 'initial': 'center', px: 2.5 }}>
+
+                        <Avatar sx={{ minWidth: 0, mr: props.drawerOpen ? 3 : 'auto', justifyContent: 'center' }}>{props.userInfo ? props.getInitals(props.userInfo.first_name, props.userInfo.last_name): 'XX'}</Avatar>
+                        <ListItemText primary={ props.userInfo ? props.userInfo.username : 'Fetching'} sx={{ opacity: props.drawerOpen ? 1 : 0 }} />
+
+                    </ListItemButton>
+
+                </ListItem>
+
+            </List>
+
+
+
+        </Drawer>
+
+    );
+
+}
+
+function Tab(props:ITab) {
+
+    return (
+
+        <ListItem disablePadding sx={{display: 'block'}}>
+
+        <ListItemButton sx={{ minHeight: 48, justifyContent: props.drawerOpen ? 'inital': 'center', px: 2.5 }} selected={props.currentTab === 3} onClick={() => props.handleTabClick(props.tab.tab_number)}>
+
+            <ListItemIcon sx={{ minWidth: 0, mr: props.drawerOpen ? 3 : 'auto', justifyContent: 'center' }}>{props.tab.icon}</ListItemIcon>
+            <ListItemText primary={props.tab.title} sx={{ opacity: props.drawerOpen ? 1 : 0 }} />
+
+        </ListItemButton>
+
+        </ListItem>
+
+    );
+
+}
--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import { useAuth } from '../hooks/useAuth';
-import { APIResponse } from '../types/APIResponse';
-import axios, { Axios, AxiosResponse } from 'axios';
-import { Box, Typography } from '@mui/material';
+import axios, { AxiosResponse } from 'axios';
+import { Box, CircularProgress } from '@mui/material';
+import DashboardDrawer from '../components/Dashboard/DashboardDrawer';
 
 /* Define Types and Interfaces */
 export interface IUserInfo {
@@ -19,7 +19,7 @@ export interface IUserInfo {
 export default function Dashboard() {
 
   /* Define State Variables */
-  const [drawerOpen, setDrawerOpen]              = useState<boolean>(false);                 // Drawer open or closed state
+  const [drawerOpen, setDrawerOpen]              = useState<boolean>(true);                 // Drawer open or closed state
   const [fetchingUserInfo, setFetchingUserInfo]  = useState<boolean>(false);                 // State that is true while fetching user info and false when we have it
   const [userInfo, setUserInfo]                  = useState<IUserInfo>();                    // Stores the user's infomation
 
@@ -32,6 +32,14 @@ export default function Dashboard() {
 
   /* Get the logout function from the global auth state */
   const { logout } = useAuth();
+
+  /* Function to change the current tab */
+  const handleTabClick = (tab_number: number) => setCurrentTab(tab_number);
+
+  /* Function to get user's initals */
+  const getInitals = (first_name: string, last_name: string): string => {
+    return `${first_name.charAt(0)}${last_name.charAt(0)}`;
+  };
 
   /* Condionally render the current tab using the current tab state */
   const renderCurrentTab = () => {
@@ -117,26 +125,14 @@ export default function Dashboard() {
   /* Dashboard JSX */
   return (
 
-    <Box>
+    <Box sx={{ display: 'flex' }}>
 
-      { fetchingUserInfo ? (
-        
-        <>
-          <h1>User Info:</h1>
-          <p>{userInfo?.first_name}</p>
-          <p>{userInfo?.last_name}</p>
-          <p>{userInfo?.username}</p>
-          <p>{userInfo?.email}</p>
-          <p>{userInfo?.bio}</p>
-          <p>{userInfo?.following_count}</p>
-          <p>{userInfo?.follower_count}</p>
-        </>
-
-      ) : (
-
-        <h1>Fetching</h1>
-
-      ) }
+      {
+        userInfo 
+        ? (<DashboardDrawer drawerOpen={drawerOpen} setDrawerOpen={setDrawerOpen} currentTab={currentTab} setCurrentTab={setCurrentTab} handleTabClick={handleTabClick} userInfo={userInfo} fetchUserData={fetchUserData} fetchingUserInfo={fetchingUserInfo} getInitals={getInitals} />) 
+        : (<WaitingSpinner />)
+      }
+      
 
     </Box>
 
@@ -144,256 +140,12 @@ export default function Dashboard() {
 
 }
 
+function WaitingSpinner() {
 
+  return (
 
+    <CircularProgress color="success" />
 
+  );
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { useCallback, useEffect, useState } from 'react';
-// import { styled, useTheme, Theme, CSSObject } from '@mui/material/styles';
-// import Box from '@mui/material/Box';
-// import MuiDrawer from '@mui/material/Drawer';
-// import MuiAppBar, { AppBarProps as MuiAppBarProps } from '@mui/material/AppBar';
-// import DashboardAppBar from '../components/Dashboard/DashboardAppBar';
-// import DashboardDrawer from '../components/Dashboard/DashboardDrawer';
-// import FixedThemeToggleButton from '../components/utils/FixedThemeToggleButton';
-// import Home from '../components/Dashboard/Home';
-// import Search from '../components/Dashboard/Search';
-// import FollowingFeed from '../components/Dashboard/FollowingFeed';
-// import ForYouFeed from '../components/Dashboard/ForYouFeed';
-// import Settings from '../components/Dashboard/Settings';
-// import axios from 'axios';
-// import { APIResponse } from '../types/APIResponse';
-// import { CircularProgress } from '@mui/material';
-// import { useAuth } from '../hooks/useAuth';
-
-// /* Interfaces */
-// export interface AppBarProps extends MuiAppBarProps {
-//   open?: boolean;
-// }
-
-// export interface DashboardAppBarProps {
-//     open: boolean;
-//     setOpen: (open: boolean) => void;
-// }
-
-// export interface DashboardDrawerProps {
-//     open: boolean;
-//     setOpen: (open: boolean) => void;
-//     selectedTab: number;
-//     setSelectedTab: (selectedTab: number) => void;
-//     userInfo: any;
-//     fetchUserInfo: () => void;
-// }
-
-// /* Define Constants */
-
-// export const drawerWidth = 240;
-
-// /* Mixins */
-
-// export const openedMixin = (theme: Theme): CSSObject => ({
-
-//     width: drawerWidth,
-//     transition: theme.transitions.create('width', {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.enteringScreen,
-//     }),
-//     overflowX: 'hidden',
-
-// });
-  
-// export const closedMixin = (theme: Theme): CSSObject => ({
-
-//     transition: theme.transitions.create('width', {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-//     overflowX: 'hidden',
-//     width: `calc(${theme.spacing(7)} + 1px)`,
-//     [theme.breakpoints.up('sm')]: {
-//       width: `calc(${theme.spacing(8)} + 1px)`,
-//     },
-
-// });
-
-// /* Custom Components */
-
-// export const DrawerHeader = styled('div')(({ theme }) => ({
-
-//     display: 'flex',
-//     alignItems: 'center',
-//     justifyContent: 'flex-end',
-//     padding: theme.spacing(0, 1),
-//     ...theme.mixins.toolbar
-
-// }));
-
-// export const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
-    
-//     ({ theme, open }) => ({
-
-//       width: drawerWidth,
-//       flexShrink: 0,
-//       whiteSpace: 'nowrap',
-//       boxSizing: 'border-box',
-
-//       ...(open && {
-//         ...openedMixin(theme),
-//         '& .MuiDrawer-paper': openedMixin(theme),
-//       }),
-
-//       ...(!open && {
-//         ...closedMixin(theme),
-//         '& .MuiDrawer-paper': closedMixin(theme),
-
-//       }),
-//     })
-
-// );
-
-// export const AppBar = styled(MuiAppBar, { shouldForwardProp: (prop) => prop !== 'open', })<AppBarProps>(({ theme, open }) => ({
-    
-//     zIndex: theme.zIndex.drawer + 1,
-//     transition: theme.transitions.create(['width', 'margin'], {
-//       easing: theme.transitions.easing.sharp,
-//       duration: theme.transitions.duration.leavingScreen,
-//     }),
-
-//     ...(open && {
-
-//       marginLeft: drawerWidth,
-//       width: `calc(100% - ${drawerWidth}px)`,
-
-//       transition: theme.transitions.create(['width', 'margin'], {
-//         easing: theme.transitions.easing.sharp,
-//         duration: theme.transitions.duration.enteringScreen,
-//       }),
-
-//     }),
-
-// }));
-
-// /* Dashboard Component */
-
-// export default function MiniDrawer() {
-
-//     {/* Define State */}
-//     const [open, setOpen] = useState<boolean>(false);                 // Drawer open state
-//     const [selectedTab, setSelectedTab] = useState<number>(0);        // Selected tab state
-//     const [userInfo, setUserInfo] = useState<any>(null);              // Stores the user info
-
-//     {/* Get the logout from authState */}
-//     const { logout } = useAuth();
-
-//     {/* Determine what component to show */}
-//     const renderContent = () => {
-
-//       switch(selectedTab) {
-
-//         case 0:
-//             return <Home userInfo={userInfo} fetchUserInfo={fetchUserInfo} />;
-//         case 1:
-//             return <Search />;
-//         case 2:
-//             return <FollowingFeed />;
-//         case 3:
-//             return <ForYouFeed />;
-//         case 4:
-//             return <Settings userInfo={userInfo} />;
-//         default:
-//             return <Home userInfo={userInfo} fetchUserInfo={fetchUserInfo} />;
-
-//       }
-
-//     };
-
-//     /* Fetch user info function */
-//     const fetchUserInfo = useCallback(async () => {
-//       try {
-//         {/* Retrieve the auth token from local storage */}
-//         const token = localStorage.getItem('token');
-
-//         if (!token) {
-//           logout();
-//         }
-
-//         {/* Make API call to get user info */}
-//         const response: APIResponse = await axios.get('/api/user/get-user-info', {
-//           headers: {
-//             Authorization: `Bearer ${token}`
-//           }
-//         });
-
-//         {/* Store the user info */}
-//         setUserInfo(response.data.data);
-
-//       } catch (error) {
-//         console.log(`Error fetching user data: ${error}`);
-//         logout();
-//       }
-//     }, [logout]);
-
-//     {/* Get the user info on page load */}
-//     useEffect(() => {
-
-//       fetchUserInfo();
-
-//     }, [fetchUserInfo]);
-
-//     {/* Dashboard JSX */}
-//     return (
-
-//         <Box sx={{ display: 'flex' }}>
-            
-//             <DashboardAppBar open={open} setOpen={setOpen}  />
-//             <DashboardDrawer open={open} setOpen={setOpen} selectedTab={selectedTab} setSelectedTab={setSelectedTab} userInfo={userInfo} fetchUserInfo={fetchUserInfo} />
-
-//             <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-//               <DrawerHeader />
-//               {renderContent()}
-//             </Box>
-
-//             <FixedThemeToggleButton />
-
-//         </Box>
-//     );
-// }
-
-// /* 
-
-
-
-// */
+}
